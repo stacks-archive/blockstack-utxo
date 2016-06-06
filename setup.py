a90873a7d75cfb2c8607cdf535eeb0593cf6f7f3
Jude Nelson <judecn@gmail.com>
2016-06-06 15:27:02 -0400

    add service dependencies

--- setup.py
+++ setup.py
@@ -12,14 +12,17 @@ setup(
     license='MIT',
     author='Blockstack.org',
     author_email='support@blockstack.org',
-    description='Python library for interacting with various UTXO providers',
+    description='Python library for interacting with various blockchain service providers',
     keywords='blockchain bitcoin btc cryptocurrency name key value store data',
     packages=find_packages(),
     download_url='https://github.com/blockstack/blockstack-utxo/archive/master.zip',
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        'pybitcoin>=0.9.8'
+        "python-bitcoinrpc>=0.1",
+        "requests>=2.4.3",
+        "virtualchain>=0.0.13.0",
+        "bitcoin>=1.1.42"
     ],
     classifiers=[
         'Intended Audience :: Developers',
