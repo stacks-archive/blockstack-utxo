6433797222b8a4f33d366d68e8c6f867688a902c
Jude Nelson <judecn@gmail.com>
2016-06-06 15:26:41 -0400

    treat blockstack as its own service provider

---
+++ blockstack_utxo/blockstack/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    blockstack-utxo
+    ~~~~~
+
+    :copyright: (c) 2014-2015 by Halfmoon Labs, (c) 2016 by Blockstack.org
+    :license: MIT, see LICENSE for more details.
+"""
+
+import blockstack_utxo
